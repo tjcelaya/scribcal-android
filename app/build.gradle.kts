65afd6c7cea9d475ab5c76b3efda75b2980e2335
Tomas Celaya <tjcelaya@gmail.com>
2025-10-24 20:17:59 -0700

    minor fixes, version code bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         applicationId = "com.tjcelaya.scribcal"
         minSdk = 34
         targetSdk = 36
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
